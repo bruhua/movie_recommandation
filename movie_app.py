c8afef725a3dd72944e3bd601dbdf0b3abe58023
bruhua <99339351+bruhua@users.noreply.github.com>
2022-09-29 10:51:15 +0200

    Update movie_app.py

--- movie_app.py
+++ movie_app.py
@@ -28,18 +28,36 @@ PAGES = {
 # Fonction pour charger les données
 @st.cache(allow_output_mutation=True)
 def load_data1():
-    df = pd.read_csv('https://raw.githubusercontent.com/bruhua/movie_recommandation/main/Dataframe/df_filtre.csv')
-    return df
+    df_graph = pd.read_csv('https://raw.githubusercontent.com/bruhua/movie_recommandation/main/Dataframe/df_graph.csv')
+    return df_graph
 
 @st.cache(allow_output_mutation=True)
 def load_data2():
     final_film = pd.read_csv('https://raw.githubusercontent.com/bruhua/movie_recommandation/main/Dataframe/Final%20film.csv')
     return final_film
 
+@st.cache(allow_output_mutation=True)
+def load_data3():
+    df_exemple = pd.read_csv('https://raw.githubusercontent.com/bruhua/movie_recommandation/main/Dataframe/df_exemple.csv')
+    return df_exemple
+
+@st.cache(allow_output_mutation=True)
+def load_data4():
+    df_overview = pd.read_csv('https://raw.githubusercontent.com/bruhua/movie_recommandation/main/Dataframe/df_overview.csv')
+    return df_overview
+
+@st.cache(allow_output_mutation=True)
+def load_data5():
+    df_meta = pd.read_csv('https://raw.githubusercontent.com/bruhua/movie_recommandation/main/Dataframe/df_meta.csv')
+    return df_meta
+
 
 # Chargement des données
-df = load_data1()
+df_graph = load_data1()
 final_film = load_data2()
+df_exemple = load_data3()
+df_overview = load_data4()
+df_meta = load_data5()
 
 
 # Sidebar
@@ -50,7 +68,7 @@ st.sidebar.title("Système de recommandations de films")
 # Choix de la page
 selection = st.sidebar.radio("", list(PAGES.keys()),key=range(0,3) )
 page = PAGES[selection]
-page.app(df, final_film)
+page.app(df_graph, final_film, df_exemple,df_overview,df_meta)
 
 # PROFIL LINKEDIN
 st.sidebar.markdown("""<hr style="height:2px;color:#333;background-color:#333;" /> """, unsafe_allow_html=True)
